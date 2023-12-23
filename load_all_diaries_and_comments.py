f44d00febe043daced855495104fd8ee4df2706f
Ken-2511 <IWMAIN@outlook.com>
2023-12-22 23:04:48 -0500

    initial commit

---
+++ load_all_diaries_and_comments.py
@@ -0,0 +1,42 @@
+import os
+
+diary_dir = r"The\directory\of\your\diaries"
+
+
+def diary_sort_key(dir_name):
+    nums = dir_name.split('-')
+    return [int(n) for n in nums]
+
+
+def get_time_str(dir_name):
+    y, mon, d, h, m, s = [int(i) for i in dir_name.split('-')]
+    return f"(date: {y}.{mon}.{d}, time: {h}.{m})"
+
+
+def remove_blank_lines(content: str):
+    while True:
+        index = content.find('\n\n')
+        if index == -1:
+            break
+        content = content[:index] + content[index+1:]
+    return content
+
+
+def load_all():
+    content = ""
+    diary_list = os.listdir(diary_dir)
+    diary_list.sort(key=diary_sort_key)
+    for dir_name in diary_list:
+        content += get_time_str(dir_name) + '\n'
+        with open(os.path.join(diary_dir, dir_name, "diary.txt"), "r", encoding="utf-8") as file:
+            content += remove_blank_lines(file.read())
+        content += '\n\n'
+        with open(os.path.join(diary_dir, dir_name, "comment.txt"), "r", encoding="utf-8") as file:
+            content += remove_blank_lines(file.read())
+        content += '\n\n'
+    with open("all_diaries_and_comments.txt", "w", encoding="utf-8") as file:
+        file.write(content)
+
+
+if __name__ == '__main__':
+    load_all()
---
+++ word_frequency.py
@@ -0,0 +1,36 @@
+import os
+
+word_len = 5
+
+
+def load_all_diaries():
+    diary_dir = r"The\directory\of\your\diaries"
+    all_content = ""
+    for folder_name in os.listdir(diary_dir):
+        file_name = os.path.join(diary_dir, folder_name, "diary.txt")
+        with open(file_name, "r", encoding="utf-8") as file:
+            content = file.read()
+            all_content += content
+    return all_content
+
+
+def count_frequency(all_diaries: str):
+    all_words = dict()
+    for i in range(len(all_diaries)-word_len+1):
+        word = all_diaries[i:i+word_len]
+        if all_words.get(word) is None:
+            all_words[word] = 0
+        all_words[word] += 1
+    # convert the dictionary to list
+    ans = list(all_words)
+    for i in range(len(ans)):
+        ans[i] = ans[i], all_words[ans[i]]
+    ans.sort(key=lambda a: a[1], reverse=True)
+    return ans
+
+
+if __name__ == '__main__':
+    all_diaries = load_all_diaries()
+    frequency = count_frequency(all_diaries)
+    for word, freq in frequency[:30]:
+        print(word, freq)
