d836381ded1a9dec711294464c3c996530bc5bbc
Ken-2511 <IWMAIN@outlook.com>
2023-12-27 14:29:43 -0500

    Update

---
+++ config.py
@@ -0,0 +1,3 @@
+diary_dir = "/home/iwmain/Documents/diaries"
+text_app = "mousepad"
+model = "gpt-4-1106-preview"
--- create_diary.py
+++ create_diary.py
@@ -1,7 +1,6 @@
 import os
 from datetime import datetime
-
-diary_dir = r"The\directory\of\your\diaries"
+from config import *
 
 now = datetime.now()
 dir_name = os.path.join(diary_dir, f"{now.year}-{now.month}-{now.day}-{now.hour}-{now.minute}-{now.second}")
@@ -9,4 +8,4 @@ os.mkdir(dir_name)
 diary_name = os.path.join(dir_name, f"diary.txt")
 with open(diary_name, "w", encoding="utf-8") as file:
     pass
-os.system(f"notepad {diary_name}")
+os.system(f"{text_app} {diary_name}")
