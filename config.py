6cb225c5bb0c4dd51f71782022328e6f5f9b160d
Ken Cheng <IWMAIN@outlook.com>
2024-01-31 23:50:23 -0500

    update to make it works both on windows and linux

--- config.py
+++ config.py
@@ -1,3 +1,20 @@
-diary_dir = "/home/iwmain/Documents/diaries"
-text_app = "mousepad"
-model = "gpt-4"
+import os
+
+# check whether it is windows or linux
+if os.name == 'nt':
+    is_windows = True
+else:
+    is_windows = False
+
+if is_windows:
+    diary_dir = r"C:\Users\IWMAI\OneDrive\Personal-Diaries"
+    text_app = "notepad"
+    model = "gpt-4"
+else:
+    diary_dir = "/home/iwmain/Documents/diaries"
+    text_app = "mousepad"
+    model = "gpt-4"
+
+
+if __name__ == '__main__':
+    print(diary_dir, text_app, model)
