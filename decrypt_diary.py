5acce4a48e1dda15d661fc1379571d8c60503d4d
Ken <yongkang.cheng@mail.utoronto.ca>
2025-08-12 16:38:27 -0400

    ....

--- decrypt_diary.py
+++
@@ -1,9 +0,0 @@
-import utils
-import os
-
-diary_name = r"C:\Users\IWMAI\OneDrive\Personal-Diaries\2025-01-15-08-11-46\diary.txt"
-
-with open(diary_name, "r", encoding="utf-8") as file:
-	content = file.read()
-	content = utils.process_secrets(content, "decrypt")
-	print(content)
